01ca4d9f98d0ea246a47bfc608ba26eace71788c
YoungsonMan <choi13570@naver.com>
2024-09-29 22:39:17 +0900

    [Feat] 잔디 Ecnounter 추가
    잔디에서 일단 디버그로그 띄우는걸로 나오는거 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerController : MonoBehaviour
 {
-    
+
+    // [SerializeField] Rigidbody2D rigid;
+    // private float x;
+    // private float y;
+
+
     [SerializeField] float moveSpeed;
     [SerializeField] float runSpeed;
     [SerializeField] bool isMoving;
@@ -15,6 +20,8 @@ public class PlayerController : MonoBehaviour
 
     // LayerMask 이용해서 레이어로 못가는곳 지정하기.
     [SerializeField] LayerMask restrictedLayer;
+    [SerializeField] LayerMask grassLayer;
+
 
 
     private static int moveRight = Animator.StringToHash("playerWalkRight");
@@ -48,6 +55,16 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
+
+            // 맵에서 점프로 넘어가는 부분에서 플랫포머를 쓰려는데 리지드 바디가 없어서 안가지는데
+            // 위에 움직이는 방식을 바꾸거나 다른방식으로 넘어가는거를 해야되거나 함.
+            //   x = Input.GetAxisRaw("Horizontal");
+            //   y = Input.GetAxisRaw("Vertical");
+            //   rigid.AddForce(Vector2.right * x * moveSpeed, ForceMode2D.Force);
+            //   rigid.AddForce(Vector2.up * y * moveSpeed, ForceMode2D.Force);
+
+
+
             // no diagonal move
             if (input.x != 0) input.y = 0;
 
@@ -58,7 +75,7 @@ public class PlayerController : MonoBehaviour
                 targetPos.y += input.y;
 
                 if (IsWalkable(targetPos))
-                    StartCoroutine(Move(targetPos));    
+                    StartCoroutine(Move(targetPos));
             }
         }
     }
@@ -72,7 +89,33 @@ public class PlayerController : MonoBehaviour
         }
         transform.position = targetPos;
         isMoving = false;
+
+        CheckForEncounters();
+
     }
+    private void CheckForEncounters()
+    {
+        // TileCollider에 CompositeCollider만쓰면 안된다 ... 다시된다...
+        
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer) != null)
+        {
+            // 1~100까지 생성 하는수에서 50보다 낮으면 걸리는건데 너무 안걸리는데. 50:50인건데
+            // 걸릴떈 또 확걸린다. 좀 뭔가 그럴사한 확률을 만들어야될거같다.
+            if (Random.Range(1, 101) <= 50) 
+            {
+                Debug.Log("야생의 포켓몬이 튀어나왔다!");
+            }
+        }
+    }
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.3f, restrictedLayer) != null)
+        {
+            return false;
+        }
+        return true;
+    }
+
     private void AnimatorPlay()
     {
         int checkAniHash;
@@ -119,18 +162,11 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private bool IsWalkable(Vector3 targetPos)
-    {
-        if (Physics2D.OverlapCircle(targetPos, 0.3f, restrictedLayer) != null)
-        {
-            return false;
-        }
-        return true;
-    }
+
 
     void Start()
     {
-        
+
     }
 
 
